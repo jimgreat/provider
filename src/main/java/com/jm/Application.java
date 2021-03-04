78e4ed9fc64755681594b9066c66327bdb103bcf
jinming <jinming@xiaomi.com>
2021-03-04 13:50:56 +0800

    new

--- src/main/java/com/jm/Application.java
+++ src/main/java/com/jm/Application.java
@@ -1,10 +1,12 @@
 package com.jm;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
+@MapperScan("com.jm.business.mapper")
 @EnableDubbo(scanBasePackages = "com.dubbo.back")
 public class Application  {
     public static void main(String[] args) {
